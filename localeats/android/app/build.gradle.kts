945060c9d1662ee32d3aad447ce2d9beca9bfe3c
Shahriar Fahim <msfahim71291@gmail.com>
2026-05-06 09:52:06 +0600

    Update build.gradle.kts

--- localeats/android/app/build.gradle.kts
+++ localeats/android/app/build.gradle.kts
@@ -2,32 +2,39 @@ plugins {
     id("com.android.application")
     id("com.google.gms.google-services")
     id("kotlin-android")
-    id("dev.flutter.flutter-gradle-plugin")
 }
+
 android {
     namespace = "com.localeats.localeats"
     compileSdk = 34
-    ndkVersion = "25.0.8775105"
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_17.toString()
     }
+
     defaultConfig {
         applicationId = "com.localeats.localeats"
-        minSdk = flutter.minSdkVersion
+        minSdk = 21
         targetSdk = 34
         versionCode = 1
         versionName = "1.0.0"
     }
+
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("debug")
+            isMinifyEnabled = false
         }
     }
 }
-flutter {
-    source = "../.."
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.22")
 }
+
+apply(from = "${rootProject.projectDir}/../flutter/packages/flutter_tools/gradle/flutter.gradle")
