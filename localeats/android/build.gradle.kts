0c2125b1840bfd7f2673c9905536ad860b435b8f
Shahriar Fahim <msfahim71291@gmail.com>
2026-05-06 10:16:07 +0600

    Update build.gradle.kts

--- localeats/android/build.gradle.kts
+++ localeats/android/build.gradle.kts
@@ -1,33 +1,18 @@
-plugins {
-    id("com.android.application")
-    id("com.google.gms.google-services")
-    id("kotlin-android")
-    id("dev.flutter.flutter-gradle-plugin")
-}
-android {
-    namespace = "com.localeats.localeats"
-    compileSdk = 34
-    ndkVersion = "25.0.8775105"
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
-    }
-    defaultConfig {
-        applicationId = "com.localeats.localeats"
-        minSdk = 21
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
-    }
-    buildTypes {
-        release {
-            signingConfig = signingConfigs.getByName("debug")
-        }
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
     }
 }
-flutter {
-    source = "../.."
+
+rootProject.buildDir = file("../build")
+subprojects {
+    project.buildDir = file("${rootProject.buildDir}/${project.name}")
+}
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
 }
